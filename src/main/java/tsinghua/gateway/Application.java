b674d87f629b681271c14535bfe87df196cd3a1c
hi1summer <gonick@163.com>
2020-09-09 11:20:34 +0800

    add hystrix filter

--- src/main/java/tsinghua/gateway/Application.java
+++ src/main/java/tsinghua/gateway/Application.java
@@ -2,10 +2,21 @@ package tsinghua.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/fallback")
+    ResponseEntity<String> errorPage() {
+        return new ResponseEntity<String>("Error",
+                HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
