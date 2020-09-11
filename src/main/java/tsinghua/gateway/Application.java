af0d41fa1e91ec6bfe9f32fb364bf6fad6a346d1
hi1summer <gonick@163.com>
2020-09-11 11:35:02 +0800

    SPRING BOOT APP->SPRING CLOUD APP

--- src/main/java/tsinghua/gateway/Application.java
+++ src/main/java/tsinghua/gateway/Application.java
@@ -1,7 +1,7 @@
 package tsinghua.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
-@SpringBootApplication
+@SpringCloudApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
